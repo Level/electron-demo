da394e5d0a094a8ae5b865aeefa339cb501905fc
Lars-Magnus Skog <ralphtheninja@riseup.net>
2017-08-15 01:42:23 +0200

    add standard

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require('app')
+var BrowserWindow = require('browser-window')
 
-var mainWindow = null;
+var mainWindow = null
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadUrl('file://' + __dirname + '/index.html') // eslint-disable-line
 
-  mainWindow.openDevTools();
+  mainWindow.openDevTools()
 
-  mainWindow.on('closed', function() { mainWindow = null });
-});
-
+  mainWindow.on('closed', function () { mainWindow = null })
+})
